4e8cc0e45cff0fec899eabfd5c7e24c52f38d53b
Ludwig Andreas <landreas-pc@ludwig-mac-pro.Dlink>
2023-08-05 19:09:18 +0300

    added MVP implementation of MetricSystem core

---
+++ test/properties_reader_class_test.cc
@@ -0,0 +1,45 @@
+#include <fstream>
+#include <sstream>
+#include <map>
+
+#include "test.h"
+
+#include "PropertiesReader.h"
+
+// Fixture
+class FileReadingTest : public ::testing::Test {
+ protected:
+  static void writeTestFile(const std::string& filePath, const std::string& content) {
+    std::ofstream outputFile(filePath);
+    outputFile << content;
+    outputFile.close();
+  }
+};
+
+TEST_F(FileReadingTest, properties_reader_basic_handler) {
+  std::string filePath = "test_file.properties";
+  std::string fileContent = "agent.name=cpu_agent_name_linux\n"
+                            "agent.type=cpu_agent\n"
+                            "agent.update_time_ms=350\n"
+                            "agent.metrics=[\"cpu\", \"processes\"]\n"
+                            "agent.metrics.cpu.return=double\n"
+                            "agent.metrics.cpu.critical_value=\">=20\"\n"
+                            "agent.metrics.processes.return=\"double\"\n"
+                            "agent.metrics.processes.critical_value=\">=20\"";
+  writeTestFile(filePath, fileContent);
+
+  std::map<std::string, std::string> expectedKeyValueMap = {
+      {"agent.name", "cpu_agent_name_linux"},
+      {"agent.type", "cpu_agent"},
+      {"agent.update_time_ms", "350"},
+      {"agent.metrics", "[\"cpu\", \"processes\"]"},
+      {"agent.metrics.cpu.return", "double"},
+      {"agent.metrics.cpu.critical_value", ">=20"},
+      {"agent.metrics.processes.return", "double"},
+      {"agent.metrics.processes.critical_value", ">=20"}
+  };
+
+  std::map<std::string, std::string> actualKeyValueMap = s21::properties_reader(filePath);
+
+  EXPECT_EQ(actualKeyValueMap, expectedKeyValueMap);
+}
